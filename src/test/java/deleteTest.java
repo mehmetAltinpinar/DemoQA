edc99e28f7e55b6e77ffaeefbb6cbaffc3886fb5
mehmetAltinpinar <mehmetu7@gmail.com>
2023-02-26 16:12:33 -0500

    src added deleteTest class added 3 lines were added to the deleteTest class

--- src/test/java/deleteTest.java
+++ src/test/java/deleteTest.java
@@ -1,2 +1,7 @@
 public class deleteTest {
+    public static void main(String[] args) {
+        System.out.println("mehmet feature verification 1");
+        System.out.println("mehmet feature verification 2");
+        System.out.println("mehmet feature verification 3");
+    }
 }
