32f156eba0a37b933044e5bed83ca6e2240ca599
mehmetAltinpinar <mehmetu7@gmail.com>
2023-02-26 17:24:34 -0500

    deleteTest class deleted

--- src/test/java/deleteTest.java
+++
@@ -1,8 +0,0 @@
-public class deleteTest {
-    public static void main(String[] args) {
-        System.out.println("mehmet feature verification 1");
-        System.out.println("mehmet feature verification 2");
-        System.out.println("mehmet feature verification 3");
-        System.out.println("mehmet feature verification 4");
-    }
-}
