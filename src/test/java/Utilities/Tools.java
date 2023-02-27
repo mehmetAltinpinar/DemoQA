671fb254e5bd3184f5bb8ccd43930169617ccac8
mehmetAltinpinar <mehmetu7@gmail.com>
2023-02-26 20:49:31 -0500

    Utilities package was added BaseDriver class was added under the Utilities package Tools class was added under the Utilities package

--- src/test/java/Utilities/Tools.java
+++ src/test/java/Utilities/Tools.java
@@ -1,20 +1,19 @@
 package Utilities;
 
 public class Tools extends BaseDriver {
-    public static void quitDriver(int time){
+    public static void quitDriver(int time) {
         try {
-            Thread.sleep(time*1000);
+            Thread.sleep(time * 1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
         driver.quit();
     }
 
-    public static void Wait(double seconds){
+    public static void Wait(double seconds) {
         try {
-            Thread.sleep((int)(seconds * 1000));
-        }
-        catch (InterruptedException e){
+            Thread.sleep((int) (seconds * 1000));
+        } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
